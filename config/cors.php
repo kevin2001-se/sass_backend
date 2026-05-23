61bed4d2d77e5e5c56d74430d32a345a585fe541
kevin2001-se <63561198+kevin2001-se@users.noreply.github.com>
2026-05-23 11:09:03 -0500

    Commit cors

--- config/cors.php
+++ config/cors.php
@@ -7,7 +7,7 @@ return [
 
     'allowed_origins' => array_filter(array_map(
         'trim',
-        explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://127.0.0.1:5174'))
+        explode(',', env('CORS_ALLOWED_ORIGINS', 'http://localhost:5173,http://127.0.0.1:5173,http://localhost:5174,http://127.0.0.1:5174,https://sass-react-46p7t3nmo-kevin-blas-projects.vercel.app'))
     )),
 
     'allowed_origins_patterns' => [],
